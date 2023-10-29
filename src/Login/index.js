892f0a35d161b9786acb7b71b07f13db7b9a335e
gopal <siriveligopal1998@gmail.com>
2023-10-29 08:45:40 +0000

    solution

--- src/Login/index.js
+++ src/Login/index.js
@@ -21,7 +21,7 @@ const Button = styled.button`
   color: #fff;
   padding: 10px 20px;
   border-radius: 10px;
-  width: 150px;
+  width: 200px;
   cursor: pointer;
 `;
 
@@ -55,10 +55,23 @@ const H = styled.p`
   color: black;
 `;
 
-const Checkbox = styled.input`
-  width: 100%;
-  padding: 10px;
-  margin: 10px 0;
+const Name = styled.p`
+  color: black;
+  font-size: 15px;
+`;
+
+const Image = styled.img`
+  height: 30px;
+  width: 30px;
+  margin: 5px;
+  margin-left: auto;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
 `;
 
 const Login = () => {
@@ -69,14 +82,24 @@ const Login = () => {
         <label htmlFor="name">Login ID</label>
         <Input type="text" id="name" placeholder="Enter Login ID" />
         <label htmlFor="password">Password</label>
-        <Input type="password" placeholder="Enter Password" id="password" />
+        <Container>
+          <Input type="password" placeholder="Enter Password" id="password" />
+          <Image
+            src="https://res.cloudinary.com/dz7osr4ia/image/upload/v1698567625/icon_mh3qba.png"
+            alt="img"
+          />
+        </Container>
         <Checkboxcontainer>
-          <Checkbox type="checkbox" />
-          Remember Me
+          <input type="checkbox" />
+          <Name>Remember Me</Name>
           <P>Change Password</P>
         </Checkboxcontainer>
-        <Checkbox type="checkbox" />
-        Agree to<Span>Terms & Conditions</Span>
+        <Checkboxcontainer>
+          <input type="checkbox" />
+          <Name>
+            Agree to<Span>Terms & Conditions</Span>
+          </Name>
+        </Checkboxcontainer>
       </Inputcontainer>
 
       <Button>Login</Button>
